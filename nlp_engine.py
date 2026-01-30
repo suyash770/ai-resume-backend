e174f9abda4d1df95744ae56283c7cbf668d7c77
Suyash Gupta <guptasuyash770@gmail.com>
2026-01-30 11:13:43 +0530

    remove spacy render fix

--- nlp_engine.py
+++ nlp_engine.py
@@ -1,27 +1,13 @@
-import spacy
-
-nlp = spacy.load("en_core_web_sm")
-
-# Skill database
 SKILLS_DB = [
-    "python", "java", "c++", "machine learning", "data science", "sql",
-    "html", "css", "javascript", "flask", "django", "react", "node",
-    "mongodb", "git", "docker", "aws", "deep learning"
+    "python", "java", "c++", "machine learning", "data science",
+    "sql", "html", "css", "javascript", "flask", "django",
+    "react", "git", "docker", "aws"
 ]
 
 def extract_skills(text):
     text = text.lower()
-    doc = nlp(text)
-
-    found_skills = set()
-
-    for token in doc:
-        if token.text in SKILLS_DB:
-            found_skills.add(token.text)
-
-    # Check multi-word skills manually
+    found = []
     for skill in SKILLS_DB:
         if skill in text:
-            found_skills.add(skill)
-
-    return list(found_skills)
+            found.append(skill)
+    return list(set(found))
