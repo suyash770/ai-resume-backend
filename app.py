e04362c52de32f7582b9c8086eea8df4937a87d6
Suyash Gupta <guptasuyash770@gmail.com>
2026-01-31 13:45:37 +0530

    pdf ats final

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from nlp_engine import extract_skills
+from pdf_reader import extract_text_from_pdf
 
 app = Flask(__name__)
 CORS(app)
@@ -13,24 +14,23 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    # Get data from frontend
-    data = request.get_json(force=True)
+    jd_text = request.form.get("jd_text", "")
 
-    resume_text = data.get("resume_text", "")
-    jd_text = data.get("jd_text", "")
+    # Get resume text from PDF
+    if "resume_pdf" in request.files:
+        pdf_file = request.files["resume_pdf"]
+        resume_text = extract_text_from_pdf(pdf_file)
+    else:
+        resume_text = ""
 
-    # Extract skills from resume and JD
     resume_skills = extract_skills(resume_text)
     job_skills = extract_skills(jd_text)
 
-    # Find matched and missing skills
     matched = list(set(resume_skills) & set(job_skills))
     missing = list(set(job_skills) - set(matched))
 
-    # Calculate ATS score safely
     score = int((len(matched) / len(job_skills)) * 100) if job_skills else 0
 
-    # Send result back to frontend
     return jsonify({
         "score": score,
         "resume_skills": resume_skills,
